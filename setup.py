4d7c25abd316d4699f0454def1fd06d5be5e5020
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2025-08-08 16:17:25 -0700

    setup.py

--- setup.py
+++
@@ -1,3 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(name="Batch text file word replacer", executables=[Executable("Batch text file word replacer script.py")], options={"build_exe": {"excludes": ["tkinter"]}})
